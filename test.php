2e58258965c70cdd6732f15d9d01a77a845b3e63
Dan Homorodean <dan.homorodean.work@gmail.com>
2025-07-21 16:49:05 +0300

    Added norroc to the people

--- test.php
+++ test.php
@@ -5,3 +5,8 @@ print 'LOTO 6/49'.PHP_EOL;
 for ($i = 0; $i < 6; $i++) {
     echo random_int(1, 49).' ';
 }
+
+print '---------------------'.PHP_EOL;
+
+echo 'Noroc'.PHP_EOL;
+echo random_int(100000, 999999).PHP_EOL;
